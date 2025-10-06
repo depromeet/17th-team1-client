7261032044b8eb8f15e29111b95ba9c6a7f8eb80
yuminnnnnni <ka09023@ajou.ac.kr>
2025-10-06 22:30:02 +0900

    chore: 카카오 로그인 아이콘 변경

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,4 +1,5 @@
 export { default as CloseIcon } from "./close.svg";
+export { default as KakaoIcon } from "./kakao.svg";
 export { default as PlusIcon } from "./plus.svg";
 export { default as ReturnIcon } from "./return.svg";
 export { default as SearchIcon } from "./search.svg";

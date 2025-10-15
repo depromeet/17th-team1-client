866243f22e2934845f2dbc1baed7cc6f52e443f7
yuminnnnnni <ka09023@ajou.ac.kr>
2025-10-15 21:45:01 +0900

    refactor: CityItem 전체 영역을 클릭 가능하게 변경

--- src/components/nationSelect/CityItem.tsx
+++ src/components/nationSelect/CityItem.tsx
@@ -1,5 +1,4 @@
 import { CloseIcon, PlusIcon } from "@/assets/icons";
-import { Button } from "@/components/common/Button";
 import type { City } from "@/types/city";
 
 type CityItemProps = {
@@ -21,23 +20,21 @@ export const CityItem = ({ city, isSelected, onAdd, onRemove, showDivider }: Cit
 
   return (
     <div>
-      <div className="flex items-center justify-between py-[18px]">
-        <button
-          type="button"
-          className={`font-medium cursor-pointer text-left ${isSelected ? "text-[#66717D]" : "text-text-primary"}`}
-          onClick={handleClick}
-        >
+      <button
+        type="button"
+        onClick={handleClick}
+        className="w-full flex items-center justify-between py-[18px] cursor-pointer"
+      >
+        <span className={`font-medium text-left ${isSelected ? "text-[#66717D]" : "text-text-primary"}`}>
           {city.flag} {city.name}, {city.country}
-        </button>
-        <Button
-          variant="gray"
-          size="xs"
-          onClick={handleClick}
-          className={`w-6 cursor-pointer items-center justify-center ${isSelected ? "bg-transparent" : ""}`}
+        </span>
+        <div
+          className={`inline-flex justify-center items-center rounded-md p-1 h-6 text-xs w-6 text-white ${isSelected ? "bg-transparent" : "bg-[#293949]"
+            }`}
         >
           {isSelected ? <CloseIcon width={10} height={10} /> : <PlusIcon width={10} height={10} />}
-        </Button>
-      </div>
+        </div>
+      </button>
       {showDivider && <div className="border-b border-surface-placeholder--8" />}
     </div>
   );

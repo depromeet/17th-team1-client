a04f5744cdee9376b2a6e0204d91015dc199ac3e
김민우 <kmw106933@naver.com>
2026-04-28 00:24:57 +0900

    fix: 빌드 시점에 외부 API 호출안되도록 변경

--- src/app/nation-select/page.tsx
+++ src/app/nation-select/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic'; 
+
 import { NationSelectClient } from "@/components/nationSelect/NationSelectClient";
 import { fetchCities } from "@/services/cityService";
 
